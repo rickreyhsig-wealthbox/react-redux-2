13fe474f05e8b085af756e4a290e415e025ea84b
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-06-17 16:38:37 -0400

    JSX content

--- src/index.js
+++ src/index.js
@@ -15,14 +15,64 @@ const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
 function App() {
-  // let message = "Bye there!";
-  // if (Math.random() > 0.5) {
-  //   message = "Hello there!";
-  // }
+  let message = "Bye there!";
+  if (Math.random() > 0.5) {
+    message = "Hello there!";
+  }
 
-  let message = [1,2,3];
+  const currentDate = new Date().toLocaleDateString();
+  const currentTime = new Date().toLocaleTimeString();
+
+  // let message = [1,2,3]; or {} objects can work as props.
   // return <h1>{message}</h1>;
-  return <h1>{message}</h1>;
+  return (
+    <div>
+      <h1>{message}</h1>
+      <p>Today's date is {currentDate}</p>
+      <p>Current time is {currentTime}</p>
+    </div>
+  )
 }
 
-root.render(<App />); 
+function App2() {
+  const name = 'Ricardo Kreyhsig';
+  const age = 30;
+
+  return (
+    <div>
+      My name is: {name} and my age is {age}
+    </div>
+  );
+}
+
+function App3() {
+
+  return (
+    <input style={{ border: '1px solid red' }} type="number" min="1" max="4" />
+  );
+}
+
+function App4() {
+  return <textarea readOnly={false} />;
+  // return <input maxLength={6} />;
+  // return <input spellCheck={true} />;
+  // return <div className="my-class" />;
+  // return <input style={{ border: '1px solid blue' }}  />;
+  // return <input style={{ backgroundColor: 'gray' }} />;
+}
+
+function App5() {
+  return (
+    <div className="wrapper">
+      <textarea
+        readOnly={true}
+        maxLength={3}
+        spellCheck={true}
+        style={{ backgroundColor: 'gray' }}
+      />
+    </div>
+  );
+}
+
+
+root.render(<App5 />);
