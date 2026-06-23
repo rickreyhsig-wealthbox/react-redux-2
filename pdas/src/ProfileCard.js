b72de2e7c5b7a5f65f15812c45e6d33fd4f34c11
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-06-23 16:46:45 -0400

    Troubleshooting props

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,16 @@
-function ProfileCard(props) {
+function ProfileCard({ title, handle }) {
+	console.log(title, handle)
+	// const title = props.title;
+	// const handle = props.handle;
+
+	// const { title, handle } = props;
+
+	// 3 Destructure in props
+
   return (
 		<div>
-			<h3>Title is {props.title}</h3>
-			<p>Handle is {props.handle}</p>
+			<h3>Title is {title}</h3>
+			<p>Handle is {handle}</p>
 		</div>
 	)
 }
