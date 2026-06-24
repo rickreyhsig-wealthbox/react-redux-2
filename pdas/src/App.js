993ba18cf433b5ed03cb2f6e0d30044d703f1fad
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-06-24 13:03:18 -0400

    Local and external images

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -3,14 +3,11 @@ import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-console.log(AlexaImage)
-console.log(CortanaImage)
-console.log(SiriImage)
-
 function App() {
   return (
     <div>
 			<h2>Personal Digital Assistants</h2>
+      {/* <img src='https://picsum.photos/200/300' alt="external" />  */}
 			<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
 			<ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
 			<ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
