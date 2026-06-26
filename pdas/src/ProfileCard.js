2e096bb3f30c5fd16920094544e39cf36624f7a7
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-06-26 11:01:50 -0400

    Adding bulma

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -2,10 +2,19 @@ function ProfileCard({ title, handle, image }) {
 	// console.log(title, handle, image)
 
   return (
-		<div>
-			<h3>Title is {title}</h3>
-			<p>Handle is {handle}</p>
-			<img src={image} alt={title} />
+		<div className="card">
+			<div className="card-image">
+				<figure className="image is-1by1">
+					<img src={image} alt='pda logo' />
+				</figure>
+			</div>
+
+			<div className="card-content">
+				<div className="media-content">
+					<p className="title is-4">{title}</p>
+					<p className="subtitle is-6">{handle}</p>
+				</div>
+			</div>
 		</div>
 	)
 }
