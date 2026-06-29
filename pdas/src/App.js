920d3c304ea769c1f2e9bee58ea758003fb73e99
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-06-29 17:28:39 -0400

    Use Bulma in HTML

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -9,9 +9,22 @@ function App() {
     <div>
 			<h2>Personal Digital Assistants</h2>
       {/* <img src='https://picsum.photos/200/300' alt="external" />  */}
-			<ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-			<ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-			<ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+            </div>
+            <div className="column is-4">
+              <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
