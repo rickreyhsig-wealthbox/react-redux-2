9865e5b3561f74b336c84be1753d1f6fcef7eb72
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-02 15:02:54 -0400

    Use state

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  }
+
   return <div>
-    Show animal list here!
-    <br />
-    <button onClick={() => console.log('Add Animal button clicked')}>
+    <button onClick={handleClick}>
       Add Animal
     </button>
+    <div>
+      Number of animals: {count}
+    </div>
   </div>
 }
 
