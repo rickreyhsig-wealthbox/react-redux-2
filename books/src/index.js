9a47ce855445ad2656d902039943fa88e9037c39
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-13 17:08:25 -0400

    CSS

--- books/src/index.js
+++ books/src/index.js
@@ -1,10 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import './index.css';
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import App from './App';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const el = document.getElementById('root')
+const root = ReactDom.createRoot(el)
+root.render(<App />);
