f9ff0012283d53b468658204ae856ddbc44d9343
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-14 13:40:50 -0400

    Update state with a new array

--- books/src/App.js
+++ books/src/App.js
@@ -5,8 +5,9 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log('Need to add book with title:', title);
-  }
+    const updatedBooks = [...books, { id: 123, title }];
+    setBooks(updatedBooks);
+  };
 
   return (
     <div>
