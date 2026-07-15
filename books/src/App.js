ea66819c0988bf895694645001b6cb6b24acd04b
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-15 13:28:51 -0400

    Display book list

--- books/src/App.js
+++ books/src/App.js
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+
 
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    const updatedBooks = [...books, { id: 123, title }];
+    const updatedBooks = [...books, { id: Math.round(Math.random()*9999), title }];
     setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} />
       <BookCreate onCreate={createBook} />
     </div>
   );
