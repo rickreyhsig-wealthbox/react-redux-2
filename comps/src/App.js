2a621d407acc69f5006441d114bc1eba32206d2e
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-21 15:19:50 -0400

    rest of props

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,10 +1,25 @@
 import Button from './Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
 
 function App() {
   return <div>
-    <div><Button success rounded text="Click me!" /></div>
-    <div><Button primary>text</Button></div>
-    <div><Button danger text="Another">Another default</Button></div>
+    <div>
+      <Button success rounded>
+        <GoBell />
+        Click Me!!
+      </Button>
+    </div>
+    <div>
+      <Button primary>
+        <GoCloudDownload />
+        Click Me!!
+      </Button>
+    </div>
+    <div><Button danger text="Another">
+      <GoDatabase />
+      Another default
+      </Button>
+    </div>
     <div><Button warning outline>Yet another default</Button></div>
     <div><Button warning>Yellow</Button></div>
     <div><Button secondary rounded>One more default</Button></div>
