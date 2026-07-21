2759f875530a9e816af7ab40defa8c435706c284
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-21 13:58:14 -0400

    Tailwind merge and finish button component

--- comps/src/App.js
+++ comps/src/App.js
@@ -2,11 +2,12 @@ import Button from './Button';
 
 function App() {
   return <div>
-    <div><Button success primary secondary text="Click me!" /></div>
-    <div><Button>Default text</Button></div>
-    <div><Button>Another default</Button></div>
-    <div><Button>Yet another default</Button></div>
-    <div><Button>One more default</Button></div>
+    <div><Button success rounded text="Click me!" /></div>
+    <div><Button primary>text</Button></div>
+    <div><Button danger text="Another">Another default</Button></div>
+    <div><Button warning outline>Yet another default</Button></div>
+    <div><Button warning>Yellow</Button></div>
+    <div><Button secondary rounded>One more default</Button></div>
 
   </div>
 }
--- comps/src/Button.js
+++ comps/src/Button.js
@@ -1,4 +1,13 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
+import { twMerge } from 'tailwind-merge';
+
+const finalClassName = className({
+  'px-1.5': true,
+  'bg-blue-500': true,
+  'text-yellow-500': true,
+});
+
+console.log(finalClassName);
 
 function Button({ children,
   text,
@@ -10,18 +19,24 @@ function Button({ children,
   outline,
   rounded 
 }) {
+  const classes = twMerge(
+    className('px-3 py-1.5 border', {
+      'border-blue-500 bg-blue-500 text-white': primary,
+      'border-gray-900 bg-gray-900 text-white': secondary,
+      'border-green-500 bg-green-500 text-white': success,
+      'border-yellow-400 bg-yellow-400 text-white': warning,
+      'border-red-500 bg-red-500 text-white': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger
+    })
+  );
 
-  return <button>{children || text}</button>;
+  return <button className={classes}>{children || text}</button>;
 }
-
-Button.propTypes = {
-  checkVariationValue: ({primary, secondary, success, warning, danger}) => {
-    const count = Number(!!primary) + Number(!!secondary) + Number(!!success) + Number(!!warning) + Number(!!danger);
-    // console.log(count)
-    if (count > 1) {
-      return new Error('Only one of primary, secondary, success, warning, danger can be true');
-    }
-  }
-};
 
 export default Button;
