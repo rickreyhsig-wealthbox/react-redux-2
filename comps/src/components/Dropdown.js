0ac11ac80e88c02d1b07f55139241454b522ea4b
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-23 16:02:05 -0400

    dropdown funct

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,13 +1,37 @@
-function Dropdown({options}) {
+import { useState } from 'react';
+
+function Dropdown({options, selection, onSelect}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+
+    onSelect(option);
+
+  }
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} value={option.value} onClick={() => handleOptionClick(option)}>
+        {option.label}
+      </div>
+    );
+  });
+
   return (
     <div>
-      <select>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+      <div onClick={handleClick}>
+        {selection?.label || 'Select...'}
+      </div>
+      {isOpen && (
+        <div>
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
