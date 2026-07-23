7c4600d7d35d9acc76ca840a32d5f45f499f09cd
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-23 14:50:02 -0400

    Add Dropdown component and AccordionPage

---
+++ comps/src/components/Dropdown.js
@@ -0,0 +1,15 @@
+function Dropdown({options}) {
+  return (
+    <div>
+      <select>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Dropdown;
