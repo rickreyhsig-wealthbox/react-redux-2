fa0e0856ae2266ba7a60db451ca00dae59875dc6
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-27 14:48:32 -0400

    Use context and provider

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,29 +1,5 @@
-import { useState } from 'react';
-import Dropdown from './components/Dropdown';
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    { label: 'Red', value: 'red' },
-    { label: 'Green', value: 'green' },
-    { label: 'Blue', value: 'blue' }
-  ];
-
-  return (
-    <div>
-      <a href="/dashboard">Dashboard</a>
-      <Dropdown
-        options={options}
-        value={selection}
-        onChange={handleSelect}
-      />
-    </div>
-  )
+  return <div></div>
 }
 
 export default App;
