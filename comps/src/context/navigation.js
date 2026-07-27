77fd8213f4c86f39a8cef0f810298ffe1462a79e
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-27 14:49:21 -0400

    Listen to back and forward clicks

---
+++ comps/src/context/navigation.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from 'react';
+
+const NavigationContext = createContext();
+
+const NavigationProvider = ({ children }) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handler);
+
+    return () => {
+      window.removeEventListener('popstate', handler);
+    };
+  }, []);
+
+
+  return (
+    <NavigationContext.Provider value={{ currentPath }}>
+      {currentPath}
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export { NavigationProvider };
+export default NavigationContext;
