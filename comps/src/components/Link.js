a393711342d60e14138b69833c59140bb52a3c5b
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-27 15:35:51 -0400

    Link style and open new tab with cntl or cmd

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,15 +1,26 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
 
+  const classes = classNames('text-blue-500', 'hover:text-blue-700', 'underline');
+
   const handleClick = (event) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
     navigate(to);
   };
 
-  return <a href={to} onClick={handleClick}>{children}</a>;
+  return <a 
+    href={to}
+    onClick={handleClick}
+    className={classes}>
+    {children}
+  </a>;
 }
 
 export default Link;
