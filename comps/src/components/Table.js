cbf62923184ba81647d5abf487b0f06fe42b9d72
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-28 20:29:32 -0400

    Add table

---
+++ comps/src/components/Table.js
@@ -0,0 +1,28 @@
+function Table({ data }) {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}> 
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Fruits</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        { renderedRows }
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
