b3d72893249d3684c5e623cb4d9f4952760ca821
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-30 17:07:16 -0400

    Add Sortable Table

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from '../components/Table';
+import SortableTable from '../components/SortableTable';
 
 function TablePage() {
   const data = [
@@ -12,7 +12,8 @@ function TablePage() {
   const config = [
     {
       label: 'Name',
-      render: (fruit) => fruit.name
+      render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name
     },
     {
       label: 'Color',
@@ -21,9 +22,8 @@ function TablePage() {
     {
       label: 'Score',
       render: (fruit) => fruit.score,
-      header: () => <th className="bg-red-500">
-        Score
-      </th>
+      sortValue: (fruit) => fruit.score,
+
     },
   ];
 
@@ -33,7 +33,7 @@ function TablePage() {
 
   return (
     <div>
-      <Table data={data} config={config} keyFn={keyFn} />
+      <SortableTable data={data} config={config} keyFn={keyFn} />
     </div>
   )
 }
