73f0696e8fe577f9cc745c801239f7d8cd747156
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-30 17:30:04 -0400

    Add sorting

--- comps/src/components/SortableTable.js
+++ comps/src/components/SortableTable.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Table from './Table';
 
 function SortableTable(props) {
-  const { config } = props;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+  const { config, data } = props;
 
   const handleClick = (label) => {
-    console.log(label);
+    if (sortOrder == null) {
+      setSortOrder('asc');
+      setSortBy(label);
+    } else if (sortOrder == 'asc') {
+      setSortOrder('desc');
+      setSortBy(label);
+    } else if (sortOrder == 'desc') {
+      setSortOrder(null);
+      setSortBy(null);
+    }
   };
 
   const updatedConfig = config.map((column) => {
@@ -20,7 +32,33 @@ function SortableTable(props) {
     }
   })
 
-  return <Table {...props} config={updatedConfig} />;
+  // Only sort if both sortOrder & sortBy are present
+  // Make copy of 'data' prop
+  // Find correct sortValue function and use for sorting.
+
+  let sortedData = data;
+  if (sortBy && sortOrder) {
+    const { sortValue } = config.find(column => column.label == sortBy)
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1
+
+      if (typeof valueA === 'string') {
+        return valueA.localeCompare(valueB) * reverseOrder
+      } else {
+        return (valueA - valueB) * reverseOrder
+      }
+    })
+  }
+
+  return (
+    <div>
+      {sortOrder} - {sortBy}
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  )
 }
 
 export default SortableTable;
