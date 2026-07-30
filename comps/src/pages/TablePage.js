392a1486772f00b01e96c1637ec06a45889875d9
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-07-30 16:35:52 -0400

    Use fragment

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -20,7 +20,10 @@ function TablePage() {
     },
     {
       label: 'Score',
-      render: (fruit) => fruit.score
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-500">
+        Score
+      </th>
     },
   ];
 
