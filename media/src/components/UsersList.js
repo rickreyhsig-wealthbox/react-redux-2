52d2ab4df3850249972a3d1e03df2515f084de8c
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-08-06 18:18:31 -0400

    Move hooks into own file

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,31 +1,14 @@
-import { useEffect, useState, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect  } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store/";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-
-
-function useThunk(thunk) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const dispatch = useDispatch();
-
-  const runThunk = useCallback((arg) => {
-    setIsLoading(true);
-    dispatch(thunk(arg))
-      .unwrap()
-      .catch(err => setError(err))
-      .finally(() => setIsLoading(false));
-  }, [dispatch, thunk]);
-
-  return [runThunk, isLoading, error];
-}
+import { useThunk } from '../hooks/use-thunk';
 
 function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
   const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
 
-  const dispatch = useDispatch();
   const { data } = useSelector((state) => {
     return state.users;
   });
