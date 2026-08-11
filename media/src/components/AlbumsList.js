d83274aae3b0a3ca09d26024a2dbbfbdf3268197
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-08-10 20:20:00 -0400

    Styling

--- media/src/components/AlbumsList.js
+++ media/src/components/AlbumsList.js
@@ -7,7 +7,7 @@ function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
   const [addAlbum, results] = useAddAlbumMutation();
-  console.log(results)
+
 
   const handleAddAlbum = () => {
     addAlbum(user);
@@ -15,7 +15,7 @@ function AlbumsList({ user }) {
 
   let content;
   if (isLoading){
-    content = <Skeleton times={3} />
+    content = <Skeleton className='h-10 w-full' times={3} />
   } else if (error) {
     content = <div>Error loading albums</div>
   } else {
@@ -32,9 +32,11 @@ function AlbumsList({ user }) {
 
   return (
     <div>
-      <div>
-        Albums for { user.name }
-        <Button onClick={handleAddAlbum}>
+      <div className='m-2 flex flex-row items-center justify-between'>
+        <h3 className='text-lg font-bold'>
+           Albums for { user.name }
+        </h3>
+        <Button loading={results.isLoading} onClick={handleAddAlbum}>
           + Add Album
         </Button>
       </div>
