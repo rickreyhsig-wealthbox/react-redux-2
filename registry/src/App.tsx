2047698ad3e1f863010a0f6c23cd247669edc7e4
rickreyhsig-wealthbox <ricardo.kreyhsig@wealthbox.com>
2026-09-01 12:17:52 -0400

    Update registry/src/App.tsx

--- registry/src/App.tsx
+++ registry/src/App.tsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/Root";
-import HomePage from "./pages/HomePage";
-import SearchPage from "./pages/SearchPage";
-import DetailsPage from "./pages/DetailsPage";
+import HomePage from "./pages/home/HomePage";
+import { homeLoader } from "./pages/home/homeLoader";
+import SearchPage from "./pages/search/SearchPage";
+import { searchLoader } from "./pages/search/searchLoader";
+import DetailsPage from "./pages/details/DetailsPage";
+import { detailsLoader } from "./pages/details/detailsLoader";
 
 const router = createBrowserRouter([
   {
@@ -12,14 +15,17 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+        loader: homeLoader,
       },
       {
         path: "/search",
         element: <SearchPage />,
+        loader: searchLoader,
       },
       {
         path: "/packages/:name",
         element: <DetailsPage />,
+        loader: detailsLoader,
       },
     ],
   },
